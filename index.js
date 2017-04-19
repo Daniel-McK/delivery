42a0138e1e935d422f362ab640ed8aab11fa12d3
Daniel McKittrick <mckittrickdaniel@gmail.com>
2017-04-18 21:17:39 -0400

    Main server file, serve static content

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+var port = process.env.PORT || 80;
+
+// third party request processing
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// app code
+app.use(express.static('public-dist'));
+
+// start the app
+app.listen(port);                     
+console.log('Running on port ' + port);     
