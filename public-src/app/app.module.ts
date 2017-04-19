7d2692817d42a749a8f4c4eb3686e968847ccaff
Daniel McKittrick <mckittrickdaniel@gmail.com>
2017-04-19 18:16:52 -0400

    App Component

--- public-src/app/app.module.ts
+++ public-src/app/app.module.ts
@@ -1,1 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { MaterialModule } from '@angular/material'
+
+import { AppRoutingModule } from './app.routing'
+import { AppComponent } from './app.component'
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        MaterialModule.forRoot(),
+
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ],
+    providers: [
+    ]
+})
 export class MainModule { }
