70f22791585a996a9110af46cceb20f04f1849dc
Daniel McKittrick <mckittrickdaniel@gmail.com>
2017-04-20 21:00:08 -0400

    Login page structure

--- public-src/app/app.module.ts
+++ public-src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { MaterialModule } from '@angular/material'
+import { MaterialModule, MdInputModule } from '@angular/material'
 
 import { AppRoutingModule } from './app.routing'
 import { AppComponent } from './app.component'
@@ -10,8 +11,10 @@ import { HomeModule } from './home/home.module'
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         MaterialModule.forRoot(),
+        MdInputModule,
 
         AppRoutingModule,
         HomeModule
