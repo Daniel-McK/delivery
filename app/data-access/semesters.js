3bde3f1a7066b2a1af16f5733c99e98d4fe7d43b
Daniel McKittrick <mckittrickdaniel@gmail.com>
2017-05-17 18:01:28 -0400

    add course grade to semester fetch

--- app/data-access/semesters.js
+++ app/data-access/semesters.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var q = require('q');
 var Semester = require('../models/semester');
 var Course = require('../models/course');
+var Deliverable = require('../models/deliverable');
 
 var semesterRouter = new express.Router();
 
@@ -21,13 +23,22 @@ semesterRouter.route('/default')
                 res.status(500).send(err);
                 return;
             }
-            Course.find({ semester: semester._id }, function (err, courses) {
+            Course.find({ semester: semester._id }).lean().exec(function (err, courses) {
                 if (err) {
                     res.status(500).send(err);
                     return;
                 }
                 semester.courses = courses;
-                res.json(semester);
+                
+                var summaryPromises = [];
+                semester.courses.forEach(function(course){
+                    summaryPromises.push(addCourseSummary(course));
+                })
+
+                q.all(summaryPromises)
+                    .then(function(){
+                        res.json(semester)
+                    });
             });
         });
     })
@@ -58,3 +69,36 @@ semesterRouter.route('/:semesterId')
     })
 
 module.exports = semesterRouter;
+
+// helpers
+
+function addCourseSummary(course){
+
+    var deferred = q.defer();
+
+    Deliverable.find({ course: course._id }, function (err, deliverables) {
+        if (err || deliverables.length == 0) {
+            deferred.resolve();
+            return;
+        }
+        var marked = 0;
+        var grade = 0;
+
+        for(var i = 0; i < deliverables.length; i++){
+            var deliverable = deliverables[i];
+            if(typeof(deliverable.mark) == "number"){
+                marked += deliverable.weight;
+                grade += deliverable.mark / 100 * deliverable.weight;
+            }
+        }
+        if(marked == 0){
+            deferred.resolve();
+            return;
+        }
+        course.mark = grade * 100 / marked;
+        deferred.resolve();
+    });
+
+    return deferred.promise;
+
+}
